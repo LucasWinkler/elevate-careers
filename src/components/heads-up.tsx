a407d8f54ec0534ef6bf83f38f6a577ce022e52d
Lucas Winkler <lucaswinkler@gmail.com>
2024-02-25 21:35:45 -0500

    Modified style of heads up component to be center aligned on mobile

--- src/components/heads-up.tsx
+++ src/components/heads-up.tsx
@@ -1,13 +1,11 @@
-import { Button } from "./ui/button";
-
 function HeadsUp() {
   return (
     <section className="bg-primary text-primary-foreground">
-      <div className="mx-auto max-w-7xl px-6 py-8">
-        <h2 className="mb-1 text-base font-medium leading-none tracking-tight md:mb-0 md:text-lg">
+      <div className="mx-auto max-w-7xl px-6 py-8 text-center md:text-start">
+        <h2 className="mb-1 text-base font-medium leading-none tracking-tight md:text-lg">
           Heads up!
         </h2>
-        <p className="text-sm leading-relaxed md:text-base">
+        <p className="mx-auto text-sm leading-relaxed md:mx-0 md:text-base">
           All search results are scraped from{" "}
           <a
             className="underline-offset-4 hover:underline"
@@ -17,7 +15,7 @@ function HeadsUp() {
           >
             Indeed.ca
           </a>{" "}
-          which may take up to 30-60 seconds
+          which may take up to 30-60 seconds.
         </p>
       </div>
     </section>
